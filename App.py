ab726aeb574fce558c148de75626099a7eea9d85
Ellery Ricaurte <elleryrica12@gmail.com>
2024-04-08 07:07:29 -0500

    Updating Proyect

--- App.py
+++ App.py
@@ -1,24 +1,23 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-from flask_cors import CORS
+from flask import Flask, request, redirect, render_template
+from config.db import app
 
-app = Flask(__name__)
-CORS(app)  # Habilitar CORS para toda la aplicación
-socketio = SocketIO(app, cors_allowed_origins="*") 
+# trabajar en las rutas de bluprint con respectos a las api's
+from api.UserApi import ruta_user
+from api.CategoryApi import ruta_category
+from api.TaksApi import ruta_tak
 
-@app.route('/')
+# Importar los Blueprints
+app.register_blueprint(ruta_user, url_prefix="/api")
+app.register_blueprint(ruta_category, url_prefix="/api")
+app.register_blueprint(ruta_tak, url_prefix="/api")
+
+# config el servidor
+
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return "hola"
 
-@socketio.on('message')
-def handle_message(msg):
-    print('Message:', msg)
-    socketio.emit('message', msg)
 
-@socketio.on('mensaje')
-def handle_message2(msg):
-    print('Emitiendo:', msg)
-    socketio.emit('mensaje', msg)
-
-if __name__ == '__main__':
-    socketio.run(app,debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, port=5000, host="0.0.0.0")
